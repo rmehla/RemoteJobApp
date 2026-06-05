76e9d4c0d39ab861ab9bbb6d3071fbdb604e8dfa
rmehla <rmehla@hotmail.com>
2026-06-05 21:07:45 +0530

    Initial Commit

---
+++ RemoteRecruit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteRecruit
+//
+//  Created by rmehla on 05/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
